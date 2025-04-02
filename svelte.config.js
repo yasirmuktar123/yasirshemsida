3136cd7493ccab79e7f646f2a622a555e0fa6016
Yasir Muktar <yasmuk@elevad.umea.se>
2025-04-02 12:12:15 +0200

    installation

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,26 @@
+
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [vitePreprocess(), ],
+	extensions: ['.svelte'],
+
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+			fallback: null
+		}),
+		
+	},
 };
 
 export default config;
+			
+			
